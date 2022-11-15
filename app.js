a22e46a132a21382823b1739f76dfc16a9aafefa
Ali Sawari <ali.dragon.swr@gmail.com>
2022-11-15 04:42:34 +0300

    better app!

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 import * as lib from "./lib"
+
+
+function checkAge(age){
+  age = Number(age);
+  if(typeof age === 'number'){
+    if(age < 18) return "you cant drink kiddo!"
+    if(age >= 18 && age <= 30) return "there you go! enjoy"
+    if(age > 30) return "ofc you can drink grandpa!"
+  }
+  return "wtf you doing i need some age!"
+}
+
+lib.bindClick('go', () => {
+  console.log("LOL i just got clicked")
+  const age = lib.getValue("age");
+  const response = checkAge(age);
+  lib.setValue("result", response);
+})
--- lib.js
+++ lib.js
@@ -6,3 +6,7 @@ export function bindClick(id, func){
 export function getValue(id){
   return document.getElementById(id).value;
 }
+
+export function setValue(id, value){
+  return document.getElementById(id).textContent = value;
+}
